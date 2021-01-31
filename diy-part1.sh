b7f6d71f12f4c0e27a1a97555daf4d668bbaf246
K <819355396@qq.com>
2021-01-31 19:34:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,10 +13,9 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-sed -i '$a src-git xray https://github.com/XTLS/Xray-core.git' feeds.conf.default
+# Add feed sources
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
