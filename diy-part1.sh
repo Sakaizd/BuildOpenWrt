e94f7882e1a684a4c33200ccafc8c45cadfaf061
K <819355396@qq.com>
2021-02-12 01:31:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,20 +14,16 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
-
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lean/lua-maxminddb  
 
-git clone https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
+# git clone https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
