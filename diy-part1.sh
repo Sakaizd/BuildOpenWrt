5dfb480b0cc71fd7d525268fe82373823fae9f9a
K <sakaizd@gmail.com>
2021-07-25 20:46:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,7 @@ git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
