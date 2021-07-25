f743ce70f2105e6298fa4ef9e6eeb9d41feda274
K <sakaizd@gmail.com>
2021-07-25 20:45:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,4 +25,4 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-# git clone https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git  package/lean/luci-app-vssr
