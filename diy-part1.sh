c795cba095bb4e8ef72401ff11419767d64b31d4
K <23312700+Sakaizd@users.noreply.github.com>
2021-10-15 02:25:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -64,8 +64,8 @@ rm -rf ../lean/luci-theme-argon
 # rm -rf package/lean/luci-theme-argon
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-# Add mentohust & luci-app-mentohust
-git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
-git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
+
+
+
 
 popd
