9da2b5b3a8210feef9d2204ebff51b7fe73898d8
Sakaizd <23312700+Sakaizd@users.noreply.github.com>
2022-06-29 23:44:03 +0800

    Update 2102.config
    update
    
    Update 21.02.yml
    
    Update 21.02.yml
    
    Update 21.02.yml
    
    Update 21.02.yml
    
    Create test.yml
    
    Update test.yml
    
    Update test.yml
    
    Update test.yml
    
    Update test.yml
    
    Update test.yml
    
    Update 21.02.yml
    
    update
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    update

--- scripts/immortalFeeds.sh
+++ scripts/immortalFeeds.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# add kenzok8/openwrt-packages
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
@@ -8,3 +13,4 @@ pushd package/community
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
 
 popd
+
